6d7893d1af1089efaf48380465a2cca3cab44084
dikshsawaliya <diksh101sawaliya@gmail.com>
2021-09-09 00:08:16 +0530

    Added the icon and Tips Section

--- app/src/main/java/com/northcap/gettingripped/FoodActivity.java
+++ app/src/main/java/com/northcap/gettingripped/FoodActivity.java
@@ -1,14 +1,28 @@
 package com.northcap.gettingripped;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class FoodActivity extends AppCompatActivity {
 
+   // int[] newArray;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food);
+
+       /*  Toolbar toolbar = findViewById(R.id.toolBarFood);
+        setSupportActionBar(toolbar);
+
+       newArray = new int[]
+                {
+                        R.id.tip1, R.id.tip1, R.id.tip1, R.id.tip1,R.id.tip1,R.id.tip1,R.id.tip1, R.id.tip1, R.id.tip1
+
+
+
+                };*/
     }
 }
